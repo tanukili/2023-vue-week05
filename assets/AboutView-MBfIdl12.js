ddbe29cd4507c0c5d3e6f63eb7adadc60c501359
tanukile <backpacker320@gmail.com>
2024-02-01 16:08:45 +0800

    Updates

---
+++ assets/AboutView-MBfIdl12.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t,a as s}from"./index-TLL3zd0G.js";const c={},a=s("h1",null,"This is an about page",-1),n=[a];function _(r,i){return t(),o("div",null,n)}const d=e(c,[["render",_]]);export{d as default};
